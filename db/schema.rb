c615d4874273aa4507cd1f090436f61ef913c998
hermesonbastos <hermesonbastos@gmail.com>
2025-05-09 03:58:35 -0300

    :bug: fix: remove due_date from table columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_09_064936) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_09_065718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -26,7 +26,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_09_064936) do
     t.bigint "board_id", null: false
     t.string "title", null: false
     t.text "description"
-    t.datetime "due_date"
     t.integer "position", default: 0, null: false
     t.boolean "is_done_column", default: false, null: false
     t.datetime "created_at", null: false
