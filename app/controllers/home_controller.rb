9a20e2ad862d1b6570faab999a834c83e2b88332
hermesonbastos <hermesonbastos@gmail.com>
2025-05-09 15:31:26 -0300

    :sparkles: feat: setup tailwind

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    # aqui você pode preparar qualquer variável para a view
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
+  get "home/index"
 
   get  "/signup", to: "users#new", as: :signup
   post "/signup", to: "users#create"
