70203c4c090dfb0b64575691fcb771135cb11215
hermesonbastos <hermesonbastos@gmail.com>
2025-05-09 00:06:15 -0300

    :sparkles: feat: create users table and User model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name,  presence: true
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :profile_photo,
+            length: { maximum: 1.megabyte },
+            allow_nil: true
+
+end
