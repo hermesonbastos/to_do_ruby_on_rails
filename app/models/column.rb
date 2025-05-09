13a8f763827f8407a932b04641d3ab025e445312
hermesonbastos <hermesonbastos@gmail.com>
2025-05-09 04:25:33 -0300

    :bug: fix(workflow/test): fix sintax error

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,9 +1,9 @@
 class Column < ApplicationRecord
   belongs_to :board
 
-  validates :title, presense: true
+  validates :title, presence: true
   validates :position,
-              presense: true,
+              presence: true,
               numericality: {
                 only_integer: true,
                 greater_than_or_equal_to: 0
